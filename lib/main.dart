640b9a6cef1d2c1b09880476f63c4e2baf5a5785
Tuuz <miracle-plus@outlook.com>
2020-11-18 16:38:27 +0800

    Tuuz

--- lib/main.dart
+++ lib/main.dart
@@ -29,21 +29,23 @@ class _BottomTab extends State<BottomTab> with SingleTickerProviderStateMixin {
   TabController _tabController;
 
   static const tabpage = <Widget>[
-    Center(child: Icon(Icons.cloud, size: 64, color: Colors.green)),
-    Center(child: Icon(Icons.alarm, size: 64, color: Colors.green)),
-    Center(child: Icon(Icons.forum, size: 64, color: Colors.green)),
+    Center(child: Icon(Icons.message, size: 64, color: Colors.blue)),
+    Center(child: Icon(Icons.contacts, size: 64, color: Colors.green)),
+    Center(child: Icon(Icons.supervised_user_circle, size: 64, color: Colors.green)),
+    Center(child: Icon(Icons.settings, size: 64, color: Colors.green)),
   ];
 
   static const tab = <Tab>[
-    Tab(icon: Icon(Icons.cloud), text: "tab1"),
-    Tab(icon: Icon(Icons.alarm), text: "tab1"),
-    Tab(icon: Icon(Icons.forum), text: "tab1"),
+    Tab(icon: Icon(Icons.message), text: "Message"),
+    Tab(icon: Icon(Icons.contacts), text: "Contacts"),
+    Tab(icon: Icon(Icons.supervised_user_circle), text: "Circle"),
+    Tab(icon: Icon(Icons.settings), text: "Me"),
   ];
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 3, vsync: this);
+    _tabController = TabController(length: tab.length, vsync: this);
   }
 
   @override
@@ -59,6 +61,7 @@ class _BottomTab extends State<BottomTab> with SingleTickerProviderStateMixin {
       body: TabBarView(
         children: tabpage,
         controller: _tabController,
+
       ),
       bottomNavigationBar: Material(
         color: Colors.blue,
