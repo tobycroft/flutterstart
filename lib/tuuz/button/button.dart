3a622a80ef724223ec31e5c0458d9355aff825ac
Tuuz <miracle-plus@outlook.com>
2021-01-19 16:52:40 +0800

    Tuuz

---
+++ lib/tuuz/button/button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutterstart/tuuz/win/close.dart';
+
+class Closebutton {
+  @override
+  CloseX() {
+    return Closebutton();
+  }
+
+  BackButton() {
+    return BackButton();
+  }
+
+  BackWithWord(BuildContext context) {
+    return FlatButton(
+      padding: EdgeInsets.only(right: 40),
+      child: Text(
+        "<返回",
+        style: TextStyle(color: Colors.white, fontSize: 19),
+      ),
+      onPressed: () {
+        Tuuz_win().Close(context);
+      },
+    )
+  }
+}
