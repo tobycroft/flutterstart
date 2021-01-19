d1013a19d195d7bba7f1d558dda45c1373319950
Tuuz <miracle-plus@outlook.com>
2021-01-19 17:10:46 +0800

    Tuuz

--- lib/tuuz/win/close.dart
+++ lib/tuuz/win/close.dart
@@ -5,4 +5,12 @@ class Tuuz_win {
   Close(BuildContext context) {
     return Navigator.pop(context);
   }
+
+  Open(BuildContext context, StatefulWidget any) {
+    return Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext contexts) => any));
+  }
+
+  Open_static(BuildContext context, StatelessWidget any) {
+    return Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext contexts) => any));
+  }
 }
