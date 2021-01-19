eb62b5a33feaa309184261bf0763b98c66cbd21c
Tuuz <miracle-plus@outlook.com>
2021-01-19 15:43:26 +0800

    Tuuz

--- lib/tuuz/popup/popupmenu.dart
+++ lib/tuuz/popup/popupmenu.dart
@@ -2,15 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Tuuz_Popup {
-  Menu(IconData icon, String text, String value) {
+  MenuItem(IconData icon, String text, String value) {
     return new PopupMenuItem<String>(
-        value: value,
-        child: new Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            new Icon(icon, color: Colors.blue),
-            new Text(text),
-          ],
-        ));
+      value: value,
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          new Icon(icon, color: Colors.blue),
+          new Text(text),
+        ],
+      ),
+    );
   }
 }
