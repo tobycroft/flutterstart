2bae75c333cbf9d9f104d543838d1d8fb6b732fe
Tuuz <silverhawk@foxmail.com>
2021-01-20 20:47:36 +0800

    Tuuz

--- lib/tuuz/net/net.dart
+++ lib/tuuz/net/net.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'dart:ffi';
 import 'dart:io';
 
 import 'package:flutterstart/config/app.dart';
@@ -15,18 +17,64 @@ class Net {
       uri = new Uri.http(url, path, get);
     }
     var query = new Uri(queryParameters: post);
-    var req = http.postUrl(uri);
-    req.then((resp) => {
-          if (header != null && !header.isEmpty)
-            {
-              header.forEach((key, value) {
-                resp.headers.add(key, value);
-              }),
-            },
-          resp.headers.contentType = ContentType.parse("application/x-www-form-urlencoded"),
-          resp.writeln(query.query),
-          resp.close(),
-        });
-    // var ret = resp.transform(utf8.decoder).join();
+    var req = await http.postUrl(uri);
+    if (header != null && !header.isEmpty) {
+      header.forEach((key, value) {
+        req.headers.add(key, value);
+      });
+    }
+    req.headers.contentType = ContentType.parse("application/x-www-form-urlencoded");
+    req.write(query.query);
+    var resp = await req.close();
+    var ret = await resp.transform(utf8.decoder).join();
+    return ret;
+  }
+
+  PostRaw(String url, path, Map<String, String> get, dynamic post, Map<String, String> header) async {
+    var http = new HttpClient();
+    http.findProxy = (url) {
+      return HttpClient.findProxyFromEnvironment(url, environment: {"http_proxy": Config().ProxyURL});
+    };
+    var uri;
+    if (get == null || get.isEmpty) {
+      uri = new Uri.http(url, path);
+    } else {
+      uri = new Uri.http(url, path, get);
+    }
+    var req = await http.postUrl(uri);
+    if (header != null && !header.isEmpty) {
+      header.forEach((key, value) {
+        req.headers.add(key, value);
+      });
+    }
+    req.headers.contentType = ContentType.text;
+    req.write(post);
+    var resp = await req.close();
+    var ret = await resp.transform(utf8.decoder).join();
+    return ret;
+  }
+
+  PostJson(String url, path, Map<String, String> get, Map<String, dynamic> post, Map<String, String> header) async {
+    var http = new HttpClient();
+    http.findProxy = (url) {
+      return HttpClient.findProxyFromEnvironment(url, environment: {"http_proxy": Config().ProxyURL});
+    };
+    var uri;
+    if (get == null || get.isEmpty) {
+      uri = new Uri.http(url, path);
+    } else {
+      uri = new Uri.http(url, path, get);
+    }
+    var req = await http.postUrl(uri);
+    if (header != null && !header.isEmpty) {
+      header.forEach((key, value) {
+        req.headers.add(key, value);
+      });
+    }
+    req.headers.contentType = ContentType.text;
+    req.writeln(post);
+    var resp = await req.close();
+    var ret = await resp.transform(utf8.decoder).join();
+    return ret;
   }
 }
