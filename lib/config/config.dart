d993f0947d8d696b635e01bdb223214a60b8a756
Tuuz <silverhawk@foxmail.com>
2021-01-21 20:14:41 +0800

    Tuuz

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -2,7 +2,7 @@ class Config {
   double Font_Size = 24;
   double Font_size_text = 16;
 
-  String ProxyURL = "http://192.168.1.10:9000";
+  String ProxyURL = "http://10.0.0.100:9000";
 
   String Url = "api.tuuz.cn:15088";
 
