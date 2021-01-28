93a5ea924e8495b5a11ba1bf9892f247df35b391
Tuuz <miracle-plus@outlook.com>
2021-01-28 14:58:34 +0800

    Tuuz

---
+++ lib/app/robot_info/index.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Robot_info_index extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _robot_info_index();
+}
+
+class _robot_info_index extends State<Robot_info_index> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+  }
+}
